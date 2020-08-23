ccd1767ab2a82ec554ead112e70f5974cacf907d
Dustin Ramirez <dustin.r.ramirez@gmail.com>
2020-08-23 03:41:02 +0000

    Shortened code by removing stripping of port number

--- total.sh
+++ total.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cat /log/$(date +\%Y-%m-%d)-syslog.log | egrep /[2][2-3]$ | awk '{print $12}' | awk -F '/' '{print $1}' | wc -l
+cat /log/$(date +\%Y-%m-%d)-syslog.log | egrep /[2][2-3]$ | awk '{print $12}' | wc -l
